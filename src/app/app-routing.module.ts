6be82da28f203be5d183b5e15b08909b82811e7c
davidbisaccia <davidbisaccia@gmail.com>
2020-05-22 12:16:54 -0400

    Added Reports Page and Setup Routing
    Updated the header as well so the Reports link works in the nav bar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import {NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { ReportsComponent } from './reports/reports/reports.component';
 
 const appRoutes:Routes = [
     {path: '', redirectTo: '/auth', pathMatch: 'full'},
     {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
     {path: 'time', loadChildren: () => import('./time/time.module').then(m => m.TimeModule)},
-    // {path: '', redirectTo: '/recipe', pathMatch: 'full' },
-    // {path: 'recipe', loadChildren: () => import('./Recipe/recipe.module').then(m => m.RecipeModule) },
-    // {path: 'auth', loadChildren: () => import('./Auth/auth.module').then(m => m.AuthModule)},
-    // {path: 'shopping-list', loadChildren: () => import('./Shopping/shopping.module').then(m => m.ShoppingModule)},
+    {path: 'reports', component: ReportsComponent}
    ];
 
    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
+import { ReportsComponent } from './reports/reports/reports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ReportsComponent,
   ],
   imports: [
     BrowserModule,
